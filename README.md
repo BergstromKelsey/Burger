7b2db8f78a63f27c4256e045dd596daba1eb7b45
Kelsey <BergstromKelsey@gmail.com>
2021-02-10 20:40:37 -0600

    withImages

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Burger
+# Burger Saving App w/ Handlebars!
+
+This application was made to take user input, store it in the data base, and display it on the front end for the user to see. Users can enter in any name of a burger they would like to eat, and it will display in a list called "Burgers To Eat" below. Once the user eats said burger, they are able to click a button, "Ate it" which will then move the chosen burger to a list called "Burgers Eaten."
+
+This project was my first full stack application! It was also my first expierence wokring with Handlebars. I am used to single JS file, along with an HTML and CSS. This has multiple, multiple files, which took a minute to get used to!
+
+Heroku Live Link: 
+![alt text]
+![alt text]
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -14,7 +14,6 @@ $(function() {
       }).then(
         function() {
           console.log("changed burger to", newBurg);
-          // Reload the page to get the updated list
           location.reload();
         }
       );
@@ -22,29 +21,26 @@ $(function() {
 
 
     $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       var newBurg = {
         burger_name: $("#ca").val().trim(),
         devoured:0
-        // devoured: $("[burger_name=devoured]:checked").val().trim()
       };
   
-      // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurg
 
-
-
-
       }).then(
         function() {
-          console.log("created new cat");
+          console.log("created burger");
           // Reload the page to get the updated list
           location.reload();
         }
       );
      });
- });
+
+    
+  });
+ 
