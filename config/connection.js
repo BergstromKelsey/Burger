52081ddb43384dfffeb52c27ec467c0cb0495b2e
Kelsey <BergstromKelsey@gmail.com>
2021-02-11 09:50:06 -0600

    herokubugs

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL); ({
-  host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "l2h3v78vnb5y9amt",
-  password: "vhhdz5jdb6qmau1t",
-  database: "qtsrakwxn8as9481"
-});
+if (process.env.JAWSDB_URL){
+connection=mysql.createConnection(process.env.JAWSDB_URL)
+
+} else{
+  connection = mysql.createConnection ({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+
+}
+
+
 
 // Make connection.
 connection.connect(function(err) {
